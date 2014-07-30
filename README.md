239fc0412366d74532e4686ead97fac682454edc
Q11d7 <Q11d7@openmailbox.org>
2014-07-30 14:58:23 -0400

    texting aspect done

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my final project for the flatiron school
+
+This program texts you the latest Quotes from the "Humans of New York" website.
--- final_project.rb
+++ final_project.rb
@@ -1,46 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
-
-class Quote
-
-  def initialize
-    html = open("http://www.humansofnewyork.com/")
-
-    nokogiri_doc = Nokogiri::HTML(html)
-
-    quote = nokogiri_doc.css(".p1").children.first
-
-    puts "Quote obtained"
-  end
-
-end
-
 require 'rubygems' # not necessary with ruby 1.9 but included for completeness
 require 'twilio-ruby'
 
-class Text
+html = open("http://www.humansofnewyork.com/")
 
-  def initialize(sid, token, send_to, quote)
-    # put your own credentials here
-    account_sid = sid
-    auth_token = token
+nokogiri_doc = Nokogiri::HTML(html)
 
-    # set up a client to talk to the Twilio REST API
-    @client = Twilio::REST::Client.new account_sid, auth_token
+quote = nokogiri_doc.css(".p1").children.first
 
-    @client.account.messages.create(
-      :from => '+14159341234',
-      :to => send_to,
-      :body => quote
-    )
+puts "Quote obtained"
 
-    puts "Message sent"
-  end
+# put your own credentials here
+account_sid = "ACe330ba04d082392df4cb3511dcb72cec"
+auth_token = "2008ea097713e401a16c54029058da82"
 
-end
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
 
-Quote.new
-Text.new('ACe330ba04d082392df4cb3511dcb72cec', '2008ea097713e401a16c54029058da82', "+12038324748", quote)
+@client.account.messages.create(
+  :from => '+18152642023',
+  :to => "+12038324748",
+  :body => quote
+  )
+
+puts "Message sent"
+
 
 
 
